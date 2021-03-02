2498b55805a706d63db53e16f5741c3369776f95
XingCodes <fuxingchn@foxmail.com>
2021-03-02 12:38:50 +0800

    更新

--- QuantumultX/Rewrite/Zhihu/answer.js
+++ QuantumultX/Rewrite/Zhihu/answer.js
@@ -1,15 +1,15 @@
 try{
-	let body = $response.body
-	body=JSON.parse(body)
-	delete body['ad_info']
+	let body = $response.body;
+	body=JSON.parse(body);
+	delete body['ad_info'];
 	// body['data'].forEach((element, index)=> {
 	//     if(element['author']['name']=="盐选推荐"||element['author']['name']=="盐选科普"){ 
 	//           body['data'].splice(index,1)  
 	//      }
 	//  })
-	body=JSON.stringify(body)
-	$done({body})
+	body=JSON.stringify(body);
+	$done({body});
 }
 catch(err){
-	this.log(`知乎回答列表去广告出现异常：${err}`,"INFO")
+	this.log(`知乎回答列表去广告出现异常：${err}`,"INFO");
 }
