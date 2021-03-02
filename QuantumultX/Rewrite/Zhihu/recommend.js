c256559e314d5c95156170a5b5e59d801fe79cce
XingCodes <fuxingchn@foxmail.com>
2021-03-02 12:44:35 +0800

    更新

--- QuantumultX/Rewrite/Zhihu/recommend.js
+++ QuantumultX/Rewrite/Zhihu/recommend.js
@@ -1,10 +1,17 @@
 try{
 	let body = $response.body;
+	this.log('zhihu开始',"INFO");
 	body=JSON.parse(body);
-	body['data'].forEach((element, index)=> {
-	    if(element['card_type'] === 'slot_event_card' || element.hasOwnProperty('ad') || (element.hasOwnProperty('extra') && element['extra'].hasOwnProperty('type') && element['extra']['type'] === 'zvideo')){      
-	       body['data'].splice(index,1)  
-	    }
+	// body['data'].forEach((element, index)=> {
+	//     if(element['card_type'] === 'slot_event_card' || element.hasOwnProperty('ad') || (element.hasOwnProperty('extra') && element['extra'].hasOwnProperty('type') && element['extra']['type'] === 'zvideo')){      
+	//        body['data'].splice(index,1)  
+	//     }
+	// });
+	body['data'].filter((element)=> {
+	    let flag = !(element['card_type'] === 'slot_event_card' 
+            || element.hasOwnProperty('ad') 
+            || (element.hasOwnProperty('extra') && element['extra'].hasOwnProperty('type') && element['extra']['type'] === 'zvideo'));
+	    return flag;
 	});
 	body=JSON.stringify(body);
 	$done({body});
