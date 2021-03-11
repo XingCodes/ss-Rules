01d1d1831ed6b337a75547f6728a26cc4f7d7186
XingCodes <fuxingchn@foxmail.com>
2021-03-11 22:53:38 +0800

    更新

---
+++ QuantumultX/Rewrite/Zhihu/comments.js
@@ -0,0 +1,13 @@
+// https://raw.githubusercontent.com/XingCodes/ss-Rules/master/QuantumultX/Rewrite/Zhihu/comments.js
+try{
+	let body = $response.body
+	if (!!body){
+		let obj = JSON.parse(body);
+		obj['ad_info'] = {};
+		body = JSON.stringify(obj);
+		$done({body});
+	}
+}
+catch(err){
+	console.log(`去除知乎评论广告出现异常：${err}`,"INFO");
+}
